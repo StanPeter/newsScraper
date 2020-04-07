11fe8c605ef796875375e75c217368368bbe4942
stan05 <stanislav.petr7@gmail.com>
2020-04-07 02:07:24 +0200

    finished project

---
+++ hacker_news_scraper.py
@@ -0,0 +1,43 @@
+#you can change rate(the last line) to get more popular or less popular news
+
+
+#required libraries
+import requests, time, pdb, pprint
+from bs4 import BeautifulSoup
+
+#main function
+def popular_hacker_news(rate):  
+    hacker_news = []
+    skipped = 0 #counts those news without any popularity rate
+    
+    for i in range(1, 21): #has max 20 pages
+        res = requests.get("https://news.ycombinator.com/news?p=" + str(i))
+        soup = BeautifulSoup(res.text, "html.parser")
+        
+        #acquiring neccesary content from website
+        links = soup.select(".storylink")
+        subtext = soup.select(".subtext")
+        ranks = soup.select(".rank")
+        for index, item in enumerate(links):    #loop through each website
+            try:
+                name = links[index].getText()     
+                href = links[index].get("href", None)
+                vote = subtext[index].select(".score")[0].getText()
+                rank = ranks[index].getText()
+                
+                print(rank, name, vote)
+                if int(vote.split()[0])>rate: #how many people voted for the article
+                    hacker_news.append((rank, name, href, vote))
+            except IndexError as e: #those without votes
+                skipped += 1
+                print(f"ERROR {name}: skipped for not having votes")
+                
+
+        print("scraped page: " + str(i))
+        time.sleep(3)   
+
+    print("articles without votes: " + str(skipped))
+    print("total popular news " + str(len(hacker_news)))
+    return pprint.pprint(hacker_news)
+
+popular_hacker_news(500)
---
+++ readme.txt
@@ -0,0 +1,15 @@
+Hacker news scraper programme.
+
+After executing programme will scrape "https://news.ycombinator.com/news" site for the most popular news
+
+final result is a list of tuples where each has: rank, name of article, link and votes number
+
+0. create and activate an environment inside downloaded newsScraper project 
+
+1. install required libraries using "pip install -r requirements.txt"
+
+2. follow instructions in hacker_news_scraper.py to configurate the programme or just run in terminal
+with default settings (you will only the most important news as result)
+
+PS: programme will take some time to scrape all the pages
+
